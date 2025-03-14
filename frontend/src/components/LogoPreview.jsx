6163e3904c0e95324520e66719f0631b298d000c
Sanjaykanwasi <sanjaykanwasi3@gmail.com>
2025-03-14 16:49:28 +0530

    rotate

--- frontend/src/components/LogoPreview.jsx
+++ frontend/src/components/LogoPreview.jsx
@@ -1,4 +1,5 @@
 import { UpdateStorageContext } from "@/context/UpdateStorageContext";
+import { icons } from "lucide-react";
 import React, { useContext, useEffect, useState } from "react";
 
 const LogoPreview = () => {
@@ -10,16 +11,44 @@ const LogoPreview = () => {
     setStorageValue(storageData);
   }, [updateStorage]);
 
+  const Icon = ({ name, color, size, rotate }) => {
+    const LucidIcon = icons[name];
+    if (!LucidIcon) {
+      return;
+    }
+    return (
+      <LucidIcon
+        style={{
+          transform: `rotate(${rotate}deg)`,
+        }}
+        color={color}
+        size={size}
+      />
+    );
+  };
+
   return (
     <div className="flex items-center justify-center h-screen ">
-      <div className="w-[500px] h-[500px] bg-purple-200 outline-dotted outline-purple-300">
+      <div
+        style={{
+          padding: storageValue?.bgPadding,
+        }}
+        className="w-[500px] h-[500px] bg-purple-200 outline-dotted outline-purple-300"
+      >
         <div
           style={{
             borderRadius: storageValue?.bgRounded,
             background: storageValue?.bgColor,
           }}
-          className="h-full w-full "
-        ></div>
+          className="h-full w-full flex items-center justify-center "
+        >
+          <Icon
+            name={storageValue?.icon}
+            color={storageValue?.iconColor}
+            size={storageValue?.iconSize}
+            rotate={storageValue?.iconRotate}
+          />
+        </div>
       </div>
     </div>
   );
